062c73e7c17d8e0e4637fb14f2855206289db9b2
Fullstack2035 <josebrunonobre@gmail.com>
2025-03-21 17:54:39 -0300

    Delete estruturas_condicao2.py

--- estruturas_condicao2.py
+++
@@ -1,9 +0,0 @@
-tempoExperiencia = 5
-if tempoExperiencia >= 2:
-    print('nivel de conhecimento junio')
-
-elif tempoExperiencia >= 5:
-    print('tempoexperiencia menor que 2')
-    print('nivel de conhcecimento pleno')
-else:
-    print('nivel de conhecimento senio')
